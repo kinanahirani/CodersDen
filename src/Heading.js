7f4f5a6f0f65c5b099e95ae2f94049cc97d5cc01
Yash Gandhi <64217975+Yash851@users.noreply.github.com>
2021-10-30 08:48:17 +0530

    Delete src directory

--- src/Heading.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-const StyledHeader=styled.h1`
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap');
-font-size: 2.3rem;
-font-family: 'Roboto', sans-serif;
-color:#0af05e;
-margin:0px;
-`;
-export default StyledHeader;
